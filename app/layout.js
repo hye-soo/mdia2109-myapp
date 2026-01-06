bb0ba59981abce40c27550c2bf9b031649de183d
hyesara <hyesara.shim@gmail.com>
2026-01-05 16:14:55 -0800

    first commit

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <div>
+          <button>LOGO</button>
+        </div>
         {children}
       </body>
     </html>
