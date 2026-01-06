48241d1533be5145f1145ebf85c08ea047dfaafa
hyesara <hyesara.shim@gmail.com>
2026-01-05 16:23:07 -0800

    new words

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <div>
-          <button>LOGO</button>
+          <button>LOGOss</button>
         </div>
         {children}
       </body>
